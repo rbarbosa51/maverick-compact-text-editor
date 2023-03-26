8a21bdd1468a23d93a54cece0391b82626dde212
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-03-25 21:25:00 -0500

    Configured webpack.config.js file In this commit configured webpack to use InjectManifest and WebpackPWaManifest plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,11 +18,53 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Maverick Compact Text Editor',
+      }),
+     // TODO: Add InjectManifest Plugin
+     new InjectManifest({
+      swSrc: './src-sw.js',
+      swDest: 'service-worker.js'
+     }),
+     new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'Maverick Compact Text Editor',
+      short_name: 'MaverickTextEditor',
+      description: 'This is a cool text editor',
+      background_color: '#272822',
+      theme_color: '#272822',
+      start_url: './',
+      publicPath: './',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
       
     ],
 
     module: {
       rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
         
       ],
     },
