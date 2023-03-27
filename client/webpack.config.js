d7ab578131c3a6145cf992f63ead122e41fea199
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-03-26 23:32:08 -0500

    Fixed the issue on the manifest where I would get a maskable icon warning

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -55,7 +55,7 @@ module.exports = () => {
           src: path.resolve('src/images/maskable_icon.png'),
           sizes: [128,256,512],
           destination: path.join('assets', 'icons'),
-          purpose: "any maskable"
+          purpose: "maskable"
         }
       ],
     }),
