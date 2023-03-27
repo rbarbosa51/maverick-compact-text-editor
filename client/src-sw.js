95de24687cbf5395dac7cef920bbd68472ca4271
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-03-27 16:14:51 -0500

    Fixed an error where an additional cache was being created while offline

--- client/src-sw.js
+++ client/src-sw.js
@@ -30,28 +30,22 @@ warmStrategyCache({
     "/assets/icons/icon_48x48.ico",
     "/assets/icons/icon_96x96.png",
     "/assets/icons/icon_128x128.png",
+    "/assets/icons/icon_180x180.png",
+    "/assets/icons/icon_192x192.png",
+    "/assets/icons/icon_256x256.png",
+    "/assets/icons/icon_384x384.png",
+    "/assets/icons/icon_512x512.png",
   ],
   strategy: pageCache,
 });
-//offlineFallback();
 
 registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
-// TODO: Implement asset caching
 registerRoute(
   // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
   ({ request }) =>
     ["style", "script", "manifest", "worker", "image"].includes(
       request.destination
     ),
-  new StaleWhileRevalidate({
-    // Name of the cache storage.
-    cacheName: "asset-cache",
-    plugins: [
-      // This plugin will cache responses with these headers to a maximum-age of 30 days
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
-    ],
-  })
+  pageCache
 );
